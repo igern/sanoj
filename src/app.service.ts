a4cedb8ac7ad130f205f9b40799df90c88faf975
jonas <jones16511@hotmail.com>
2022-07-23 09:36:40 +0200

    check env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello sanoj';
+    return process.env.MY_SECRET || 'not working';
   }
 }
